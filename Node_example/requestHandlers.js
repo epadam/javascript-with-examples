0527fadf3ecd95f44a0a14498c6651c696384351
epadam <andrew.epyy@gmail.com>
2019-11-03 15:31:34 +0100

    add node examples

---
+++ Node_example/requestHandlers.js
@@ -0,0 +1,22 @@
+//var exec = require("child_process").exec;
+
+function start(response){
+    console.log("Request handler 'start' was called");
+    
+
+    var body = '<html>'+
+    '<head>'+
+    'meta http-equiv="Content-Type" content="'
+    
+    ;
+
+    return content;
+}
+
+function upload(){
+console.log("Request handler 'upload' was called");
+return "Hello Upload";
+}
+
+exports.start = start;
+exports.upload = upload;
